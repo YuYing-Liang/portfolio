b6ee2ac21f5ba9fb956549c084677a7216f35b4b
YuYing_Liang <yuying.liang00@gmail.com>
2020-05-31 01:34:38 -0400

    Updates

---
+++ precache-manifest.781db68d009c30fddb5d2a3253e23aa4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b48869e449125351f510a16ef8ed3a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6be8635174e8fe783f53",
+    "url": "/portfolio/static/css/main.f32c569f.chunk.css"
+  },
+  {
+    "revision": "73013bd3d608d6605467",
+    "url": "/portfolio/static/js/2.bae77b27.chunk.js"
+  },
+  {
+    "revision": "d0d6b5033fd77eb6038046252d67d3b7",
+    "url": "/portfolio/static/js/2.bae77b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be8635174e8fe783f53",
+    "url": "/portfolio/static/js/main.f75ab28a.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "28c4c7f5712d1b5ddbafcec20744ab0e",
+    "url": "/portfolio/static/media/Resume.28c4c7f5.pdf"
+  },
+  {
+    "revision": "731856eb6260fc26fe135e763c7b0a49",
+    "url": "/portfolio/static/media/contact.731856eb.jpg"
+  },
+  {
+    "revision": "88586045bac044378965829c08910156",
+    "url": "/portfolio/static/media/engsci_club.88586045.png"
+  },
+  {
+    "revision": "3c8544fc4a1d657cdb50ab14a123e9f9",
+    "url": "/portfolio/static/media/frosh2T0.3c8544fc.jpg"
+  },
+  {
+    "revision": "8c2177a359c707156509c0c078ba3a71",
+    "url": "/portfolio/static/media/profile.8c2177a3.png"
+  },
+  {
+    "revision": "5a8507d71b52c5e692e62ac7e709e6f7",
+    "url": "/portfolio/static/media/projects.5a8507d7.jpg"
+  },
+  {
+    "revision": "c1b8311197c939bd4e1d2f641b09fdb4",
+    "url": "/portfolio/static/media/rbc.c1b83111.jpg"
+  },
+  {
+    "revision": "4e713a07d805006fc1279cfd252414c3",
+    "url": "/portfolio/static/media/seismic.4e713a07.jpg"
+  },
+  {
+    "revision": "79c894662aa55e6b9aa6575862a12064",
+    "url": "/portfolio/static/media/utat.79c89466.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f3510362e191ecd3d643be30a136fe8b.js"
+  "/portfolio/precache-manifest.781db68d009c30fddb5d2a3253e23aa4.js"
 );
 
 self.addEventListener('message', (event) => {
