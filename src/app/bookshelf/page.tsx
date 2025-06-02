56f4d65e4a89fe78f72ebdd8707b3c932c088993
YuYing <yuying.liang00@gmail.com>
2025-06-01 22:32:20 -0400

    wip: book modal

--- src/app/bookshelf/page.tsx
+++ src/app/bookshelf/page.tsx
@@ -3,13 +3,17 @@ import { Navbar, type NavbarTabs } from "./(components)/navbar";
 import { useState } from "react";
 import { Bookshelf } from "./(pages)/bookshelf";
 import { Library } from "./(pages)/library";
+import { useDisclosure } from "@mantine/hooks";
+import { BookModal } from "./(components)/book";
 
 export default function BookshelfHomepage() {
   const [currentTab, setCurrentTab] = useState<NavbarTabs>("bookshelf");
+  const [opened, { open, close }] = useDisclosure(false);
 
   return (
     <>
-      <Navbar currentTab={currentTab} onTabChange={setCurrentTab} />
+      <Navbar currentTab={currentTab} onTabChange={setCurrentTab} onAddBook={open} />
+      <BookModal open={opened} onClose={close} />
       {currentTab == "bookshelf" && <Bookshelf />}
       {currentTab == "library" && <Library />}
     </>
