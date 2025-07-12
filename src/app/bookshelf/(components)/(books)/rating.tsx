a2a7a05a52f8fde33a2f4be870630c6ddbf85123
YuYing <yuying.liang00@gmail.com>
2025-07-12 15:44:19 -0400

    feat: added book modal

---
+++ src/app/bookshelf/(components)/(books)/rating.tsx
@@ -0,0 +1,30 @@
+import { type FC } from "react";
+import { Group } from "@mantine/core";
+import { DynamicTablerIcon } from "~/app/(components)/Icon";
+import { type BookRating } from "../../types";
+
+interface RatingProps {
+  rating: BookRating;
+}
+
+export const Rating: FC<RatingProps> = (props) => {
+  return (
+    <Group gap="3px" my="xs">
+      {Array(5)
+        .fill(null)
+        .map((_, i) => (
+          <DynamicTablerIcon
+            key={i}
+            name={
+              i + 1 <= Math.floor(props.rating)
+                ? "IconStarFilled"
+                : props.rating > i
+                  ? "IconStarHalfFilled"
+                  : "IconStar"
+            }
+            size={18}
+          />
+        ))}
+    </Group>
+  );
+};
